f2bcd1333fbe2d341cc0939a7654bb0be4c5ae6a
gurpsehm <gurpreetsehmbey1@gmail.com>
2022-06-29 13:32:12 -0600

    Added the ability to change color on click of grid square

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,6 +1,13 @@
 const gridContainer = document.querySelector('#gridContainer');
 
 for (let i = 0; i < 256; i++) {
+	// Creates and displays a 16 * 16 grid
 	const gridElement = document.createElement('grid');
 	gridContainer.appendChild(gridElement);
+
+	// Clicked grid element color changed to black
+	gridElement.addEventListener('click', () => {
+		gridElement.style.backgroundColor = "black";
+	});
 }
+
