ff225d9c3a28ad806db4715c55da528b8a0e0308
gurpsehm <gurpreetsehmbey1@gmail.com>
2022-06-30 13:04:04 -0600

    Grids now are able to be clicked or dragged to change color

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -4,10 +4,13 @@ for (let i = 0; i < 256; i++) {
 	// Creates and displays a 16 * 16 grid
 	const gridElement = document.createElement('grid');
 	gridContainer.appendChild(gridElement);
+	gridElement.setAttribute('draggable', true);
 
 	// Clicked grid element color changed to black
-	gridElement.addEventListener('click', () => {
-		gridElement.style.backgroundColor = "black";
-	});
+	['mousedown', 'dragover'].forEach ( evt =>
+		gridElement.addEventListener(evt, () => {
+			gridElement.style.backgroundColor = "black";
+		})
+	);
 }
 
