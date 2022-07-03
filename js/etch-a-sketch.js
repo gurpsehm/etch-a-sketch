6c75a9929e9e0dfdda3fd3d93b7edb82544b2bb7
gurpsehm <gurpreetsehmbey1@gmail.com>
2022-07-02 20:21:14 -0600

    put grid creation into function and added the ability for range slider to control grid size

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,20 +1,32 @@
 const gridContainer = document.querySelector('#gridContainer');
 
-for (let i = 0; i < 256; i++) {
-	// Creates and displays a 16 * 16 grid
-	const gridElement = document.createElement('grid');
-	gridContainer.appendChild(gridElement);
-	gridElement.setAttribute('draggable', true);
+function createGrid(size){
+	for (let i = 0; i < (size * size); i++) {
+		// Creates and displays a user selected grid
+		const gridElement = document.createElement('grid');
+		gridContainer.appendChild(gridElement);
+		gridElement.setAttribute('draggable', true);
 
-	// Clicked grid element color changed to black
-	['mousedown', 'dragover'].forEach ( evt =>
-		gridElement.addEventListener(evt, () => {
-			gridElement.style.backgroundColor = 'black';
-		})
-	);
+		// Clicked grid element color changed to black
+		['mousedown', 'dragover'].forEach ( evt =>
+			gridElement.addEventListener(evt, () => {
+				gridElement.style.backgroundColor = 'black';
+			})
+		);
 
-	clearBtn.addEventListener('click', () => {
-		gridElement.style.backgroundColor = '';
-	});
+		// Removes grid background color
+		clearBtn.addEventListener('click', () => {
+			gridElement.style.backgroundColor = '';
+		});
+	}
 }
 
+// Slider changes and displays grid size amount
+gridSize.addEventListener('input', () => {
+	rangeValue.textContent = gridSize.value + " * " + gridSize.value;
+	gridContainer.textContent = "";
+	createGrid(gridSize.value);
+});
+
+// Creates initial grid
+createGrid(gridSize.defaultValue);
